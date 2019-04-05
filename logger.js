7fe94afe6a8f5f6f2e76166a277f833e30111e8d
Stefaan Seys <stefaan.seys@onespan.com>
2019-04-05 09:24:49 +0200

    logger with timestamp

--- logger.js
+++ logger.js
@@ -1,14 +1,32 @@
 const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, label, printf } = format;
+
 const config = require('./config.js').parse();
 
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+/*
 var logger = createLogger({
     level: config.loglevel,
     format: format.combine(
+      format.timestamp({format:'MM-YY-DD'}),
       format.colorize(),
       format.splat(),
       format.simple(),
     ),
     transports: [new transports.Console()]
+});
+*/
+
+const logger = createLogger({
+  format: combine(
+    label({ label: 'right meow!' }),
+    timestamp(),
+    myFormat
+  ),
+  transports: [new transports.Console()]
 });
 
 module.exports = logger;
